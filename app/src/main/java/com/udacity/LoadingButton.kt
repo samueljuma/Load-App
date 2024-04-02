f49682d22a067be40fc4efe859c56d21f15360a1
samueljuma <samueljuma1024@gmail.com>
2024-04-02 20:50:43 +0300

    main content UI

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,10 +1,18 @@
 package com.udacity
 
 import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.graphics.RectF
+import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -13,6 +21,9 @@ class LoadingButton @JvmOverloads constructor(
     private var widthSize = 0
     private var heightSize = 0
 
+    private var buttonText = "Download"
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
     private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
@@ -20,14 +31,39 @@ class LoadingButton @JvmOverloads constructor(
     }
 
 
+
     init {
+
+        paint.textAlign = Paint.Align.CENTER
+        paint.typeface = Typeface.DEFAULT_BOLD
+        paint.textSize = resources.getDimension(R.dimen.default_text_size)
+
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.LoadingButton)
+        buttonText = typedArray.getString(R.styleable.LoadingButton_buttonText) ?: context.getString(R.string.button_name)
+        typedArray.recycle()
+
+        //New
+        setOnClickListener{
+            buttonState = ButtonState.Clicked
+        }
 
     }
 
 
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
+        // Draw Button background
+        paint.color = ContextCompat.getColor(context, R.color.colorPrimary)
+        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+
+        // Draw Text On Button Background
+        val textBounds = Rect()
+        paint.getTextBounds(buttonText,0, buttonText.length,textBounds)
+        val textBaseline = (heightSize - textBounds.height())/2f - textBounds.top
+        paint.color = Color.WHITE
+        canvas?.drawText(buttonText, (widthSize/2).toFloat(), textBaseline, paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
