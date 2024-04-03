5a7485c8e08c8f8702c51ee40a7f574f06e84992
samueljuma <samueljuma1024@gmail.com>
2024-04-03 12:45:37 +0300

    add animation logic to custom button

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -1,7 +1,9 @@
 package com.udacity
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -18,65 +20,115 @@ import kotlin.properties.Delegates
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
     private var widthSize = 0
     private var heightSize = 0
+    private var textWidth = 0f
 
-    private var buttonText = "Download"
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private var progressCircle = 0f
+    private var progressWidth = 0f
+    private val textSize = resources.getDimension(R.dimen.default_text_size)
 
-    private val valueAnimator = ValueAnimator()
+    private var buttonText: String = context.getString(R.string.button_name)
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
-
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        textAlign = Paint.Align.CENTER
+        typeface = Typeface.DEFAULT_BOLD
+        textSize = resources.getDimension(R.dimen.default_text_size)
     }
 
+    private val buttonColor = ContextCompat.getColor(context, R.color.colorPrimary)
+    private val loadingColor = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+    private val circleColor = ContextCompat.getColor(context, R.color.colorAccent)
 
+    private var valueAnimator = ValueAnimator()
+
+    /**
+     * handles different button states (Clicked, Loading, Completed)
+     * and updates the view accordingly
+     */
+    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { _, _, new ->
+        when (new) {
+            ButtonState.Loading -> {
+                buttonText = resources.getString(R.string.button_loading)
+                valueAnimator = ValueAnimator.ofFloat(0f, widthSize.toFloat()).apply {
+                    duration = 6000
+                    addUpdateListener { anim ->
+                        progressWidth = anim.animatedValue as Float
+                        progressCircle = (widthSize.toFloat() / 365) * progressWidth
+                        invalidate()
+                    }
+                    addListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator) {
+                            if (buttonState == ButtonState.Loading) {
+                                buttonState = ButtonState.Completed
+                            }
+                        }
+                    })
+                    start()
+                }
+            }
+            ButtonState.Completed -> {
+                valueAnimator.cancel()
+                progressWidth = 0f
+                progressCircle = 0f
+                buttonText = context.getString(R.string.button_name)
+                invalidate()
+
+            }
+            else -> {}
+        }
+    }
 
     init {
-
-        paint.textAlign = Paint.Align.CENTER
-        paint.typeface = Typeface.DEFAULT_BOLD
-        paint.textSize = resources.getDimension(R.dimen.default_text_size)
-
         val typedArray = context.obtainStyledAttributes(attrs, R.styleable.LoadingButton)
         buttonText = typedArray.getString(R.styleable.LoadingButton_buttonText) ?: context.getString(R.string.button_name)
         typedArray.recycle()
 
-        //New
-        setOnClickListener{
-            buttonState = ButtonState.Clicked
+        setOnClickListener {
+            if (buttonState != ButtonState.Loading) {
+                buttonState = ButtonState.Loading
+            }
         }
-
     }
 
-
-    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
-        // Draw Button background
-        paint.color = ContextCompat.getColor(context, R.color.colorPrimary)
-        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
-
-        // Draw Text On Button Background
-        val textBounds = Rect()
-        paint.getTextBounds(buttonText,0, buttonText.length,textBounds)
-        val textBaseline = (heightSize - textBounds.height())/2f - textBounds.top
-        paint.color = Color.WHITE
-        canvas?.drawText(buttonText, (widthSize/2).toFloat(), textBaseline, paint)
+        drawButtonBackground(canvas)
+        drawButtonText(canvas)
+        drawProgressCircle(canvas)
     }
 
+    private fun drawButtonBackground(canvas: Canvas?) {
+        paint.color = if (buttonState == ButtonState.Completed) buttonColor else loadingColor
+        canvas?.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+    }
+
+    private fun drawButtonText(canvas: Canvas?) {
+        val textBounds = Rect()
+        paint.getTextBounds(buttonText, 0, buttonText.length, textBounds)
+        val textBaseline = (heightSize - textBounds.height()) / 2f - textBounds.top
+        paint.color = Color.WHITE
+        textWidth = paint.measureText(buttonText)
+        canvas?.drawText(buttonText, (widthSize / 2).toFloat(), textBaseline, paint)
+    }
+
+    private fun drawProgressCircle(canvas: Canvas?) {
+        canvas?.save()
+        canvas?.translate(widthSize / 2 + textWidth / 2 + textSize / 2, heightSize / 2 - textSize / 2)
+        paint.color = circleColor
+        canvas?.drawArc(RectF(0f, 0f, textSize, textSize), 0f, progressCircle * 0.365f, true, paint)
+        canvas?.restore()
+    }
+
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
-        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
-        val h: Int = resolveSizeAndState(
-            MeasureSpec.getSize(w),
-            heightMeasureSpec,
-            0
-        )
+        val minw = paddingLeft + paddingRight + suggestedMinimumWidth
+        val w = resolveSizeAndState(minw, widthMeasureSpec, 1)
+        val h = resolveSizeAndState(MeasureSpec.getSize(w), heightMeasureSpec, 0)
         widthSize = w
         heightSize = h
         setMeasuredDimension(w, h)
     }
-
 }
